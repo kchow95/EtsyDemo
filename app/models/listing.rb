6fe3ac3f174029649eeb520ea5716337043af886
Kenneth Chow <kchow95@gmail.com>
2016-06-23 12:59:07 -0500

    fixed listing.rb bug

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,10 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
-		has_attached_file :image, default_url: "no_image.jpg",
+		has_attached_file :image, default_url: "no_image.jpg"
 	else
-  		has_attached_file :image, 
-  		default_url: "no_image.jpg",
+  		has_attached_file :image, default_url: "no_image.jpg",
   		:storage => :dropbox,
-    	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    	:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     	:path => ":style/:id_:filename"
     end
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
