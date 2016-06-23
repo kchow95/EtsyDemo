e7ee3062ec9bf7d6df03301e3bc8838885d996cf
Kenneth Chow <kchow95@gmail.com>
2016-06-23 14:27:11 -0500

    second attempt

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,14 +1,9 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.development?
-		has_attached_file :image, default_url: "no_image.jpg"
-		validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	else
   		has_attached_file :image, default_url: "no_image.jpg",
   		:storage => :dropbox,
     	:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     	:path => ":style/:id_:filename"
     	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-    end
  
 end
 
